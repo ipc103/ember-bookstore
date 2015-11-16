e278e58af850eacdbcab76ebe97eda17a4ac0aaa
ipc103 <iancandysemail@gmail.com>
2015-11-16 15:49:34 -0500

    add checkout moda

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('book');
+  },
+
+  actions: {
+    openCheckoutModal(book) {
+      this.controllerFor('application').set('showingModal', true);
+      return this.render('modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: book,
+        controller: 'application'
+      });
+    },
+    closeCheckoutModal(){
+      this.controllerFor('application').set('showingModal', false);
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
+  }
 });
